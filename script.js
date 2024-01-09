b62dbaa2c90c7db64879d7ae44e45f95ce6271bf
Joshua Alvarado <joshalva3@gmail.com>
2024-01-09 00:25:28 -0600

    code challenge 4 completed, moving onto more string methods practice and understanding functions after

--- script.js
+++ script.js
@@ -921,3 +921,35 @@ const restaurant = {
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
 
 // Coding Challenge #4
+// write a program that recieves a list of varialbe names written in underscore_case and convert them to camelcase
+// the input will come from a text area inserted into the DOM and conversion will happen when the button is pressed
+
+// Test Data
+/*
+underscore_case
+first_name
+Some_Variable
+calculate_AGE
+delayed_departure
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+// attach event handler to the button
+document.querySelector(`button`).addEventListener(`click`, function () {
+  const text = document.querySelector(`textarea`).value;
+  const rows = text.split(`\n`); // for line break
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split(`_`);
+
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+
+    console.log(`${output.padEnd(20)}${`✅`.repeat(i + 1)}`);
+  }
+});
+
+// <--------- Strings Methods Practice --------->
